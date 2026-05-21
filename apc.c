c4fe328d6520aabcfbd123ce3a9787691ca8dfd7
ThugDud <ejr78763@gmail.com>
2026-05-21 17:45:25 -0300

    Mudança do menu principal

--- apc.c
+++ apc.c
@@ -26,10 +26,16 @@ int main() {
         char nome[100];
         char senha[50];
         int idade;
+        int carrinho[12][2];
     };
 
-    struct Login p1;
-
+    struct Login p1 = {};
+    
+    for(int i = 0; i < 12; i++) {
+        p1.carrinho[i][0] = i + 1;
+        p1.carrinho[i][1] = 0;
+    }
+    
     // Print do login
     printf("Ola, seja bem vindo a loja 'A link to the future' \n");
     printf("Entre com o seu login: \n");
@@ -56,33 +62,48 @@ int main() {
     printf("  Login feito com sucesso!\n");
     printf("===============================\n\n");
 
+    char senha_censurada[50];
+
+    for (int i = 0; i < 50; i++){
+        if(p1.senha[i]){
+            senha_censurada[i] = '*';
+        }
+    }
+
     printf("Nome cadastrado: %s\n", p1.nome);
-    printf("Senha cadastrada: %s\n", p1.senha);
+    printf("Senha cadastrada: %s\n", senha_censurada);
     printf("Idade cadastrada: %d anos\n\n", p1.idade);
 
     // Print aluguel
     
     int escolha = 0;
 
-    printf("Biblioteca de jogos para se alugar: \n");
-    printf("1 - Fallout: New Vegas (XBOX 360) \n" );
-    printf("2 - The Legend of Zelda: Tiers of The Kingdom (NINTENDO SWITCH 2) \n" );
-    printf("3 - Half Life (PC) \n" );
-    printf("4 - Paper Mario The Thousand-Year Door (NINTENDO SWITCH 2) \n" );
-    printf("5 - Mother 2: Earthbound (SNES) \n" );
-    printf("6 - Lego Batman: The legacy of the dark knight \n" );
-    printf("7 - Chrono Trigger (SNES) \n" );
-    printf("8 - Mario kart World (NINTENDO SWITCH 2) \n" );
-    printf("9 - Dark Souls: Remastered (XBOX SERIES S/X)\n" );
-    printf("10 - Super Mario Galaxy 1+2 (NINTENDO SWITCH 2) \n" );
-    printf("11 - Team Fortress 2 (PC) \n" );
-    printf("12 - The Legend of Zelda: Ocarina of Time (NINTENDO SWITCH 2) \n" );
-    printf("Escolha um jogo para alugar: \n");
-    scanf("%i", &escolha);
+    printf("Bem vindo ao alugél de jogos \"A link to the future\"");
+    printf("1. Listar Jogos\n");
+    printf("2. Cadastrar Jogo\n");
+    printf("3. Buscar Jogo por ID\n");
+    printf("4. Atualizar Jogo\n");
+    printf("5. Remover Jogo\n");
+    printf("6. Sair do Sistema\n");
+    printf("Escolha uma opcao: ");
+    scanf("%d", &escolha);
 
     switch (escolha) {
         case 1: 
-            printf("Voce escolheu: Fallout: New Vegas (XBOX 360)\n"); 
+            printf("Biblioteca de jogos para se alugar: \n");
+            printf("1 - Fallout: New Vegas (XBOX 360) \n" );
+            printf("2 - The Legend of Zelda: Tiers of The Kingdom (NINTENDO SWITCH 2) \n" );
+            printf("3 - Half Life (PC) \n" );
+            printf("4 - Paper Mario The Thousand-Year Door (NINTENDO SWITCH 2) \n" );
+            printf("5 - Mother 2: Earthbound (SNES) \n" );
+            printf("6 - Lego Batman: The legacy of the dark knight \n" );
+            printf("7 - Chrono Trigger (SNES) \n" );
+            printf("8 - Mario kart World (NINTENDO SWITCH 2) \n" );
+            printf("9 - Dark Souls: Remastered (XBOX SERIES S/X)\n" );
+            printf("10 - Super Mario Galaxy 1+2 (NINTENDO SWITCH 2) \n" );
+            printf("11 - Team Fortress 2 (PC) \n" );
+            printf("12 - The Legend of Zelda: Ocarina of Time (NINTENDO SWITCH 2) \n" );
+            printf("Escolha um jogo para alugar: \n");
             break;
         case 2: 
             printf("Voce escolheu: The Legend of Zelda: Tears of The Kingdom (NINTENDO SWITCH 2)\n"); 
