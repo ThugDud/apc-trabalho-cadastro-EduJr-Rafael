5b2a35f5ea5539a037e671ba946d587def86e444
ThugDud <ejr78763@gmail.com>
2026-05-21 16:16:44 -0300

    adicionei uma colinha do struct

--- apc.c
+++ apc.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+    // struct Pessoa {
+    //     char nome[50];
+    //     int idade;
+    //     float altura;
+    // };
+
+    // struct Pessoa p1;
+
+    // p1.idade = 12;
+    // p1.altura = 1.70;
+    // strcpy(p1.nome, "Carlos");
+
+    // printf("Nome: %s\n", p1.nome);
+    // printf("idade: %d\n", p1.idade);
+    // printf("altura: %f\n", p1.altura);
+   
 
 int main() {
 
     // Variaveis
-
+    
 
     // Cadastro
 
